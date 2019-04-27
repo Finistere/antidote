d02ed9c3126cfbf1bcf58958cb9e23a545aca6f6
Benjamin Rabier <rabier.benj@gmail.com>
2019-04-27 11:15:34 +0200

    [doc] fix CHANGELOG.md

--- src/antidote/core/__init__.py
+++ src/antidote/core/__init__.py
@@ -1,4 +1,3 @@
 from .container import DependencyContainer, DependencyInstance, DependencyProvider
-from antidote.providers.service import LazyFactory
 from .injection import DEPENDENCIES_TYPE, inject
 from .proxy import ProxyContainer
