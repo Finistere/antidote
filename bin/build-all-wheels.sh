918b1915057ca364c4f96535641dd0d4a80f1e18
Benjamin Rabier <rabier.benj@gmail.com>
2020-12-09 16:41:41 +0100

    [bin] update scripts

--- bin/build-all-wheels.sh
+++ bin/build-all-wheels.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -euo pipefail
 
 PROJECT_DIR="$(dirname "$(dirname "$(readlink -f "$0")")")"
 
-rm -f "$PROJECT_DIR/wheelhouse/*" || true
+rm -f "$PROJECT_DIR"/wheelhouse/* || true
 
 for PLATFORM in manylinux2014_i686 manylinux2010_x86_64 manylinux2014_x86_64; do
+  echo -e "\e[32mStarting $PLATFORM\e[0m"
   DOCKER_IMAGE="quay.io/pypa/$PLATFORM"
   podman pull "$DOCKER_IMAGE"
   podman run --rm -it \
-    -e PLAT="$PLATFORM" \
+    -e PLATFORM="$PLATFORM" \
     -v "$PROJECT_DIR:/antidote" \
-    -v "$HOME/.cache" \
+    -v "$HOME/.cache:/root/.cache" \
     "$DOCKER_IMAGE" \
     $(if [[ "$DOCKER_IMAGE" == "*_i686" ]]; then echo "linux32"; fi) \
     /antidote/bin/docker-build-wheels.sh
